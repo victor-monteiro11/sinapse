162bb7ac3f748878888cc897da7d31bd3e923067
Victor <victorhugopvh16@gmail.com>
2024-09-01 19:38:04 -0300

    Added hour selection on markers

--- lib/adicionar_marcadores.dart
+++ lib/adicionar_marcadores.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
 
-class AddMarkerPage extends StatelessWidget {
+class AddMarkerPage extends StatefulWidget {
+  @override
+  _AddMarkerPageState createState() => _AddMarkerPageState();
+}
+
+class _AddMarkerPageState extends State<AddMarkerPage> {
+  TimeOfDay _startTime = TimeOfDay(hour: 8, minute: 0);
+  TimeOfDay _endTime = TimeOfDay(hour: 17, minute: 0);
+
+  Future<void> _selectTime(BuildContext context, bool isStart) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: isStart ? _startTime : _endTime,
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!,
+        );
+      },
+    );
+
+    if (picked != null && picked != (isStart ? _startTime : _endTime)) {
+      setState(() {
+        if (isStart) {
+          _startTime = picked;
+        } else {
+          _endTime = picked;
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +56,29 @@ class AddMarkerPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
+            Text(
+              'Início: ${_startTime.format(context)}',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => _selectTime(context, true),
+              child: Text('Escolher Horário de Início'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Término: ${_endTime.format(context)}',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => _selectTime(context, false),
+              child: Text('Escolher Horário de Término'),
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Ação para adicionar o marcador
+                // Ação para adicionar o marcador com o nome e os horários selecionados
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.teal,
